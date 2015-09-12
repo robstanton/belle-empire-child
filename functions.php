30871925f517553061f9b92be00a1e81351a9f1a
Rob Stanton <rob@robstanton.com>
2015-09-12 17:06:43 +0000

    first commit

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+ 
+// Insert valid php here
+ 
+?>
---
+++ menu-primary.php
@@ -0,0 +1,12 @@
+<button id="toggle-navigation" class="toggle-navigation">
+	<i class="fa fa-bars"></i>
+</button>
+
+<div id="menu-primary-tracks" class="menu-primary-tracks"></div>
+<div id="menu-primary" class="menu-container menu-primary" role="navigation">
+
+   
+
+    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_class' => 'menu', 'menu_class' => 'menu-primary-items', 'menu_id' => 'menu-primary-items', 'items_wrap' => '<ul id="%1$s" class="%2$s" role="menubar">%3$s</ul>', 'fallback_cb' => 'ct_tracks_wp_page_menu') ); ?>
+
+</div><!-- #menu-primary .menu-container -->
